3a924c38dbf72c86eb785859f802c85737659cfa
bagus pradipta <bagus.pradipta@hpe.com>
2023-08-02 11:54:50 +0700

    update

--- code/create-wlan.sh
+++ code/create-wlan.sh
@@ -6,7 +6,7 @@ input_ssid=$3
 input_passphrase=$4
 input_vlan=$5
 curl --request POST \
-     --url ''$api_url'/configuration/v2/wlan/'$input_group'/'$$input_wlan'' \
+     --url ''$api_url'/configuration/v2/wlan/'$input_group'/'$input_wlan'' \
      --header 'accept: application/json' \
      --header 'content-type: application/json' \
      --header 'Authorization: Bearer '$token'' \
