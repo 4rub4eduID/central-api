f8b3bdd2e3ef8f507e64d985aabddc5323123f1a
bagus pradipta <bagus.pradipta@hpe.com>
2023-08-02 12:08:37 +0700

    edit

--- code/change-wlan.sh
+++ code/change-wlan.sh
@@ -4,7 +4,7 @@ input_group=$1
 input_wlan=$2
 input_ssid=$3
 curl --request PATCH \
-     --url ''$api_url'/configuration/v2/wlan/'$input_group'/'$$input_wlan'' \
+     --url ''$api_url'/configuration/v2/wlan/'$input_group'/'$input_wlan'' \
      --header 'accept: application/json' \
      --header 'content-type: application/json' \
      --header 'Authorization: Bearer '$token'' \
